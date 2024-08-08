6cf01618236da23b58ecebd4d753e620d4d47303
DarkyDev5 <daniel.ca.pe207@gmail.com>
2024-08-08 12:58:32 -0500

    revamp-parallax

--- src/components/About-Us/Parallax.tsx
+++ src/components/About-Us/Parallax.tsx
@@ -1,58 +1,30 @@
 // components/Parallax.tsx
-import { useEffect, useRef, ReactNode } from 'react';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, ReactNode } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 import Image from 'next/image';
-
-gsap.registerPlugin(ScrollTrigger);
 
 interface ParallaxProps {
   children: ReactNode;
 }
 
 const Parallax: React.FC<ParallaxProps> = ({ children }) => {
-  const parallaxRef = useRef<HTMLDivElement>(null);
-  const skyRef = useRef<HTMLDivElement>(null);
-  const earthRef = useRef<HTMLDivElement>(null);
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"]
+  });
 
-  useEffect(() => {
-    const parallax = parallaxRef.current;
-    const sky = skyRef.current;
-    const earth = earthRef.current;
-
-    if (parallax && sky && earth) {
-      gsap.to(sky, {
-        yPercent: -20,
-        ease: "none",
-        scrollTrigger: {
-          trigger: parallax,
-          start: "top top",
-          end: "bottom top",
-          scrub: true,
-        },
-      });
-      
-      gsap.to(earth, {
-        yPercent: 15, // Cambiado a un valor positivo para mover hacia abajo
-        ease: "none",
-        scrollTrigger: {
-          trigger: parallax,
-          start: "top top",
-          end: "bottom top",
-          scrub: true,
-        },
-      });
-    }
-  }, []);
+  const skyY = useTransform(scrollYProgress, [0, 1], ['0%', '-20%']);
+  const earthY = useTransform(scrollYProgress, [0, 1], ['0%', '15%']);
 
   return (
-    <div ref={parallaxRef} className="relative min-h-screen overflow-hidden">
-      <div ref={skyRef} className="absolute inset-0 z-0 h-full w-full">
+    <div ref={ref} className="relative min-h-screen overflow-hidden">
+      <motion.div style={{ y: skyY }} className="absolute inset-0 z-0">
         <Image src="/Parallax/skyup.png" alt="Sky background" layout="fill" objectFit="cover" priority />
-      </div>
-      <div ref={earthRef} className="absolute top-0 w-full h-1/2 z-20"> {/* Cambiado de bottom-0 a top-0 */}
+      </motion.div>
+      <motion.div style={{ y: earthY }} className="absolute top-0 w-full h-1/2 z-20">
         <Image src="/Parallax/earth2.png" alt="Earth background" layout="fill" objectFit="cover" priority />
-      </div>
+      </motion.div>
       <div className="relative z-20">
         {children}
       </div>
